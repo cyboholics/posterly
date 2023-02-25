7bd7ecb6306267a91b8a1bd88ba288a8c996b276
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-02-25 14:48:05 +0530

    add dragging

--- src/components/elements/BoxElement.tsx
+++ src/components/elements/BoxElement.tsx
@@ -2,22 +2,36 @@ import {usePoster} from "@/hooks/usePoster";
 import {BoxNode} from "@/types/nodes/elements/BoxNode";
 
 export const BoxElement = ({id}: { id: string }) => {
-    const {getNodeById, isSelected, selectNode} = usePoster();
+    const {getNodeById, isSelected, selectNode, updateNode} = usePoster();
     const node = getNodeById(id) as BoxNode;
+    const startPosition = {x: 0, y: 0}
     return (
-        <div style={{
+        <div
+            draggable={true}
+            style={{
             position: "absolute",
             marginTop: `${node.position.y_pos || 0}px`,
             marginLeft: `${node.position.x_pos || 0}px`,
             translate: "-50% -50%",
             width: `${node.css.width || 100}px`,
             height: `${node.css.height || 100}px`,
-            backgroundColor: "blue",
+            backgroundColor: node.css.background_color || "blue",
             border: isSelected(id) ? "2px solid red" : "none",
             cursor: "pointer",
             zIndex: isSelected(id) ? 1 : 0
         }} onClick={() => {
             selectNode(id)
-        }}/>
+        }}
+            onDragStart={(e) => {
+                startPosition.x = e.clientX
+                startPosition.y = e.clientY
+            }}
+            onDragEnd={(e) => {
+                const x_pos = (node.position.x_pos || 0) + (e.clientX - startPosition.x)
+                const y_pos = (node.position.y_pos || 0) + (e.clientY - startPosition.y)
+                updateNode(id,node.css , {x_pos, y_pos})
+                selectNode(id)
+            }}
+        />
     )
 }
--- src/providers/PosterProvider.tsx
+++ src/providers/PosterProvider.tsx
@@ -13,10 +13,10 @@ export const PosterContext = createContext<{
 
 const PosterProvider = ({children}: { children: ReactElement }) => {
     const [poster, setPoster] = useState<Node<any>[]>([
-        new BoxNode({x_pos: 10, y_pos:10}),
-        new BoxNode({x_pos: 90, y_pos:10}),
-        new BoxNode({x_pos: 10, y_pos:90}),
-        new BoxNode({x_pos: 90, y_pos:90})
+        new BoxNode({x_pos: 10, y_pos:10}, {background_color: "red"}),
+        new BoxNode({x_pos: 90, y_pos:10}, {background_color: "green"}),
+        new BoxNode({x_pos: 10, y_pos:90}, {background_color: "blue"}),
+        new BoxNode({x_pos: 90, y_pos:90}, {background_color: "yellow"})
     ])
     const [selectedNodeId, setSelectedNodeId] = useState<string | null>(null)
     return (
