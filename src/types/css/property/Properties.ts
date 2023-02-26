812b88d263bfd7c0ba88ae085c8c5da219d0faed
Ayush Tiwari <ayushtiwari26402@gmail.com>
2023-02-26 17:03:41 +0530

    change types of nodes css

--- src/types/css/property/Properties.ts
+++ src/types/css/property/Properties.ts
@@ -2,10 +2,9 @@ import {decorations, fonts} from "@/types/css/elements/TextNodeCss";
 
 type singleDigit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 type singleHex = singleDigit | "a" | "b" | "c" | "d" | "e" | "f";
+type colorText = "black" | "white" | "red" | "green" | "blue" | "yellow" | "orange" | "purple" | "pink" | "brown" | "grey" | "gray";
 
-export type Color = `#${singleHex}${singleHex}${singleHex}`
+export type Color = `#${singleHex}${singleHex}${singleHex}` | colorText;
 export type Pixel = number;
 export type TextFont = typeof fonts[number]
 export type TextDecoration = typeof decorations[number]
-
-const color: Color = "#000";
